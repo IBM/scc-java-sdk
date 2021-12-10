8db467e38cb4d4d0ff7c72ccc879070433b805b3
Gaurav Goswami <gouravgoswami48@gmail.com>
2021-12-10 17:08:47 +0530

    migrating from codecov.io to codeclimate

--- build/publishCodeCoverage.sh
+++ build/publishCodeCoverage.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# This script will publish code coverage info for a build of the main branch
+# This script will publish code coverage info for a build of the master branch
 # or a tagged release.
 
-if [[ -n "${TRAVIS_TAG}" || "${TRAVIS_BRANCH}" == "main" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    printf ">>>>> Publishing code coverage info for branch: %s\n" ${TRAVIS_BRANCH}
-    $HOME/codecov-bash.sh -s modules/coverage-reports/target/site/jacoco-aggregate -t $CODECOV_TOKEN
-else
-    printf ">>>>> Bypassing code coverage publish step for feature branch/PR build.\n"
-fi
-   
+printf ">>>>> Publishing code coverage info\n"
+
+JACOCO_SOURCE_PATH=modules/common/src/main/java ./cc-test-reporter format-coverage modules/common/target/site/jacoco/jacoco.xml -d -o common.json -t jacoco
+JACOCO_SOURCE_PATH=modules/configuration-governance/src/main/java ./cc-test-reporter format-coverage modules/configuration-governance/target/site/jacoco/jacoco.xml -d -o configuration-governance.json -t jacoco
+JACOCO_SOURCE_PATH=modules/findings/src/main/java ./cc-test-reporter format-coverage modules/findings/target/site/jacoco/jacoco.xml -d -o findings.json -t jacoco
+JACOCO_SOURCE_PATH=modules/notifications/src/main/java ./cc-test-reporter format-coverage modules/notifications/target/site/jacoco/jacoco.xml -d -o notifications.json -t jacoco
+
+./cc-test-reporter sum-coverage common.json configuration-governance.json findings.json notifications.json -o coverage.json -d
+
+./cc-test-reporter upload-coverage --input coverage.json
