f7552ee91498f4bba3d9f6dbc8784ee80061181b
Pavan K Manda <pavanmanda@in.ibm.com>
2023-09-11 16:36:21 +0530

    fixing test script for envs
    Signed-off-by: Pavan K Manda <pavanmanda@in.ibm.com>

--- build/testScript.sh
+++ build/testScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -euo pipefail
-echo "${SCC_ENV}" | base64 -d >> v5/security_and_compliance_center_api_v3.env
+echo "${SCC_ENV}" | base64 -d >> security_and_compliance_center_api_v3.env
 make test-int
