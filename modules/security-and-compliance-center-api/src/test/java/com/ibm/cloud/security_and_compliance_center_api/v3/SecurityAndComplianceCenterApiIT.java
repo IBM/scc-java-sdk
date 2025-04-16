784d6f4bfd99ae2d043b91a7d314684829acec8e
Bryan Bosely <bbosely@us.ibm.com>
2025-04-16 15:27:36 -0400

    chore(SCC): Manual change integration test testListRulesWithPager
    Signed-off-by: Bryan Bosely <bbosely@us.ibm.com>

--- modules/security-and-compliance-center-api/src/test/java/com/ibm/cloud/security_and_compliance_center_api/v3/SecurityAndComplianceCenterApiIT.java
+++ modules/security-and-compliance-center-api/src/test/java/com/ibm/cloud/security_and_compliance_center_api/v3/SecurityAndComplianceCenterApiIT.java
@@ -2390,8 +2390,6 @@ public class SecurityAndComplianceCenterApiIT extends SdkIntegrationTestBase {
         .instanceId("acd7032c-15a3-484f-bf5b-67d41534d940")
         .limit(Long.valueOf("10"))
         .type("system_defined")
-        .search("testString")
-        .serviceName("testString")
         .sort("updated_on")
         .build();
 
@@ -2414,8 +2412,8 @@ public class SecurityAndComplianceCenterApiIT extends SdkIntegrationTestBase {
       assertEquals(allItems.size(), allResults.size());
       System.out.println(String.format("Retrieved a total of %d item(s) with pagination.", allResults.size()));
     } catch (ServiceResponseException e) {
-        fail(String.format("Service returned status code %d: %s%nError details: %s",
-          e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()));
+      fail(String.format("Service returned status code %d: %s%nError details: %s",
+        e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()));
     }
   }
 
