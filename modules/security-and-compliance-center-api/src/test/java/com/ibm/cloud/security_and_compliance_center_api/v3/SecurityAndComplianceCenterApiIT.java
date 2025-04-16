61b3ee4d64cd2d8773de8786353dac25abd854cb
Bryan Bosely <bbosely@us.ibm.com>
2025-04-16 14:42:57 -0400

    chore(SCC): Manual change integration test testListProfiles
    Signed-off-by: Bryan Bosely <bbosely@us.ibm.com>

--- modules/security-and-compliance-center-api/src/test/java/com/ibm/cloud/security_and_compliance_center_api/v3/SecurityAndComplianceCenterApiIT.java
+++ modules/security-and-compliance-center-api/src/test/java/com/ibm/cloud/security_and_compliance_center_api/v3/SecurityAndComplianceCenterApiIT.java
@@ -1060,7 +1060,6 @@ public class SecurityAndComplianceCenterApiIT extends SdkIntegrationTestBase {
         .instanceId("acd7032c-15a3-484f-bf5b-67d41534d940")
         .accountId(accountIdForReportLink)
         .limit(Long.valueOf("10"))
-        .start("testString")
         .build();
 
       // Invoke operation
@@ -1073,8 +1072,8 @@ public class SecurityAndComplianceCenterApiIT extends SdkIntegrationTestBase {
       assertNotNull(profileCollectionResult);
 
     } catch (ServiceResponseException e) {
-        fail(String.format("Service returned status code %d: %s%nError details: %s",
-          e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()));
+      fail(String.format("Service returned status code %d: %s%nError details: %s",
+        e.getStatusCode(), e.getMessage(), e.getDebuggingInfo()));
     }
   }
 
